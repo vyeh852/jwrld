dc2f6fac76262cf385d657e5ab65d67646a12862
葉睿洋 <yeruiyang@yeruiyangdeMacBook-Air.local>
2023-03-25 17:32:44 +0800

    feat: add jsDOC to specify nextjs special component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
 
+/**
+ * Next.js uses this App component to initialize pages, we can do the following things:
+ *
+ * Persist layouts between page changes
+ * Keeping state when navigating pages
+ * Inject additional data into pages
+ * Add global CSS
+ * @return {JSX.Element}
+ */
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
+/**
+ * A custom Document can update the <html> and <body> tags used to render a Page.
+ * This file is only rendered on the server.
+ * @return {JSX.Element}
+ */
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,5 +15,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
