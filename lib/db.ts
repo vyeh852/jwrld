e03bce30284fcb35dc0848ea299b6c93ff1cfe81
葉睿洋 <yeruiyang@yeruiyangdeMacBook-Air.local>
2023-03-25 20:07:00 +0800

    feat: add serverless-mysql to interact with db

---
+++ lib/db.ts
@@ -0,0 +1,30 @@
+import mysql from "serverless-mysql";
+
+type query = {
+  query: string;
+  values: Array<unknown>;
+};
+
+const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
+
+/**
+ * @param {string} query
+ * @param {Array<unknown>} values
+ */
+export default async function excuteQuery({ query, values }: query) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+}
