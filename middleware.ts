e2015621e23a9ba49ed97d8e072e3d8006d07cc8
葉睿洋 <yeruiyang@yeruiyangdeMacBook-Air.local>
2023-03-30 15:53:16 +0800

    feat: update middle to auth note page

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/api/overview", "/api/notes", "/api/categories"],
+  matcher: ["/api/overview", "/api/notes", "/api/categories", "/:noteId+"],
 };
