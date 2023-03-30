ffa7f17ec49f9dcd9261db23f269ad8508f42b83
葉睿洋 <yeruiyang@yeruiyangdeMacBook-Air.local>
2023-03-30 15:30:40 +0800

    fix: adjust fetchAction

--- actions/fetchActions.ts
+++ actions/fetchActions.ts
@@ -2,9 +2,15 @@ import { Category } from "@/domain/models/category";
 import { Note } from "@/domain/models/note";
 import axios from "axios";
 
+type Response = {
+  success: boolean;
+  message?: string;
+  data: Category[];
+};
+
 export const createNote = async ({ title }: Partial<Note>): Promise<void> => {
   try {
-    axios.post("/api/notes", { title });
+    await axios.post("/api/notes", { title });
   } catch (error) {
     throw new Error(
       "something went wrong when creating note, please try it again later"
@@ -16,10 +22,36 @@ export const createCategory = async ({
   title,
 }: Partial<Category>): Promise<void> => {
   try {
-    axios.post("/api/categories", { title });
+    await axios.post("/api/categories", { title });
   } catch (error) {
     throw new Error(
       "something went wrong when creating category, please try it again later"
+    );
+  }
+};
+
+export const updateNoteContent = async (
+  noteId: number,
+  content: string
+): Promise<void> => {
+  try {
+    await axios.put(`/api/notes/${noteId}`, { content });
+  } catch (error) {
+    throw new Error(
+      "something went wrong when updating note content, please try it again later"
+    );
+  }
+};
+
+export const getCategoryWithNote = async (): Promise<Category[]> => {
+  try {
+    const categoriesJson = await fetch("/api/overview");
+    const categories: Response = await categoriesJson.json();
+
+    return categories.data;
+  } catch (error) {
+    throw new Error(
+      "something went wrong when getting category with note, please try it again later"
     );
   }
 };
