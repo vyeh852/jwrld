82adc769c87c55d0cb4bd47103b7a7b4e6279eaa
葉睿洋 <yeruiyang@yeruiyangdeMacBook-Air.local>
2023-04-06 17:33:26 +0800

    feat: react-codemirror apply styling

--- components/note/Editor.tsx
+++ components/note/Editor.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { styled } from "@linaria/react";
 import CodeMirror from "@uiw/react-codemirror";
 import { oneDarkTheme } from "@codemirror/theme-one-dark";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { atomone } from "@uiw/codemirror-theme-atomone";
 
 const Textarea = styled.div`
   flex: 1 1 50%;
   min-width: 0;
   height: calc(100vh - 50px);
+  font-size: 15px;
+  overflow: auto;
   .cm-theme-light,
   .cm-editor {
     height: 100%;
@@ -43,7 +48,11 @@ export default function Editor({
           autocompletion: true,
           syntaxHighlighting: true,
         }}
-        extensions={[oneDarkTheme]}
+        theme={atomone}
+        extensions={[
+          markdown({ base: markdownLanguage, codeLanguages: languages }),
+          oneDarkTheme,
+        ]}
         onChange={onChangeNoteContent}
       />
     </Textarea>
